87b5d95eb8bcd1d30ea6e26bf493e583001a2e78
Tail_Straw <maeva.gr@live.fr>
2024-03-22 10:27:09 +0100

    Minor rework

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -14,42 +14,42 @@ window.onload=function() {
 
         fetchLogin(email.value, password.value);
     })
-
-    async function fetchLogin(email, password) {
-        const connect = JSON.stringify({
-            "email": email,
-            "password": password
-        });
-
-        const response = await fetch('http://localhost:5678/api/users/login',{
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: connect
-        });
-
-        const message = await response.json();
-
-        if (message["message"] == "user not found") {
-            displayAlert("warning-alert", "Utilisateur inconnu")
-        }
-
-        if (message["error"]) {
-            displayAlert("warning-alert", "Mot de passe incorrect")
-        }
-
-        if (message["token"]) {
-            const token = message["token"];
-            sessionStorage.setItem('token', token)
-            window.location.replace("index.html");
-        }
-    }
-
-    function displayAlert(alertType, alertMessage) {
-        const login = document.querySelector(".login-form");
-        const alert = document.createElement("div");
-
-        alert.classList.add("alert-box", alertType);
-        alert.innerText = alertMessage;
-        login.appendChild(alert);
-    }
 };
+
+async function fetchLogin(email, password) {
+    const connect = JSON.stringify({
+        "email": email,
+        "password": password
+    });
+
+    const response = await fetch('http://localhost:5678/api/users/login',{
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: connect
+    });
+
+    const message = await response.json();
+
+    if (message["message"] == "user not found") {
+        displayAlert("warning-alert", "Utilisateur inconnu")
+    }
+
+    if (message["error"]) {
+        displayAlert("warning-alert", "Mot de passe incorrect")
+    }
+
+    if (message["token"]) {
+        const token = message["token"];
+        sessionStorage.setItem('token', token)
+        window.location.replace("index.html");
+    }
+}
+
+function displayAlert(alertType, alertMessage) {
+    const login = document.querySelector(".login-form");
+    const alert = document.createElement("div");
+
+    alert.classList.add("alert-box", alertType);
+    alert.innerText = alertMessage;
+    login.appendChild(alert);
+}
